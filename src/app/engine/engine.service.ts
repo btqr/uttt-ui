352b021262b909d21c70f3b20f1e052e00b68332
Bartek <krolakbartlomiej@gmail.com>
2025-05-21 21:25:23 +0200

    Make node pool smaller

--- src/app/engine/engine.service.ts
+++ src/app/engine/engine.service.ts
@@ -76,7 +76,7 @@ export class EngineService {
         a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8],
         b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8],
         javaPosition.nextBoard, javaPosition.playerToMove, Math.min(100, thinkingTime - i),
-        aggresiveOptimiziations
+        (thinkingTime - i) > thinkingTime/2, aggresiveOptimiziations
       );
       await new Promise(resolve => setTimeout(resolve, 0));
     }
